005ae6ae523ef3f18c6494e33f78eeec984d0f8b
Suraj182004 <surajyaligar2004@gmail.com>
2025-07-16 22:57:12 +0530

    solved issue

--- server/src/services/authorization.service.ts
+++ server/src/services/authorization.service.ts
@@ -1,4 +1,5 @@
-import prisma from '@/lib/prisma';
+import prisma from '../lib/prisma';
+
 
 export const checkTeacherAuthorization = async (userId: string, classId: string): Promise<boolean> => {
     const course = await prisma.class.findFirst({
